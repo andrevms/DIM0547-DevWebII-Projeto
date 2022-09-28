b2e166720150c003e49aa9f6a4ebaaa563875f2a
v1d3rm3 <victorschinaider@outlook.com>
2022-09-28 13:41:19 -0300

    feat: adicionando entidades de ListaDeEstudos e Usuario

--- estuda-concursos/src/main/java/br/com/imd/projeto/web/estudaconcursos/model/ListaDeEstudos.java
+++ estuda-concursos/src/main/java/br/com/imd/projeto/web/estudaconcursos/model/ListaDeEstudos.java
@@ -1,5 +1,26 @@
 package br.com.imd.projeto.web.estudaconcursos.model;
 
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="ListaDeEstudos")
 public class ListaDeEstudos {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String nome;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    private List<Questao> questoes;        
+
 }
